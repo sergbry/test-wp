6b0f7e89f7be93ca13510ab557524f990b849400
Sergey BRYLYAKOV <sergeybrylyakov@o-s-i.org>
2022-01-08 16:35:41 +0200

    +header +footer

---
+++ wp-content/themes/test-wp/footer.php
@@ -0,0 +1,14 @@
+	</div><!-- #content -->
+
+	<footer>
+
+        footer content
+
+	</footer>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
--- wp-content/themes/test-wp/index.php
+++ wp-content/themes/test-wp/index.php
@@ -1,2 +1,26 @@
-<?php
-echo 'Hello, Developer!';
+<?php get_header(); ?>
+
+<div class="content-area">
+	<main id="main" class="site-main">
+
+		<?php
+		if ( have_posts() ) {
+
+			// Load posts loop.
+			while ( have_posts() ) {
+				the_post();
+				the_content();
+			}
+
+		} else {
+
+			// If no content, include the "No posts found" template.
+			get_template_part( 'template-parts/content/content', 'none' );
+
+		}
+		?>
+
+	</main><!-- .site-main -->
+</div><!-- .content-area -->
+
+<?php get_footer();
