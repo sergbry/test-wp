51ed5218d7cf6eed21c00ff38c044c92a03b0be9
Sergey BRYLYAKOV <sergeybrylyakov@o-s-i.org>
2022-01-10 01:57:49 +0200

    header-bar search

--- wp-content/themes/test-wp/assets/js/main.js
+++ wp-content/themes/test-wp/assets/js/main.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     let mobile_menu_close_toggler = document.querySelector('#mobile_menu_close_toggler');
+    let mobile_menu_open_toggler = document.querySelector('#mobile_menu_open_toggler');
     let mobile_menu_main = document.querySelector('#mobile-menu-main');
     let mobile_menu_parent_items = document.querySelectorAll('#mobile-menu-main .menu-item-has-children > a');
 
     mobile_menu_close_toggler.addEventListener( 'click', function () {
         mobile_menu_main.classList.remove('active');
+    });
+
+    mobile_menu_open_toggler.addEventListener( 'click', function () {
+        mobile_menu_main.classList.add('active');
     });
 
     mobile_menu_parent_items.forEach(function (element) {
