1f210c4bc64306d5c80975c7e2d0146ab6f895c9
Sergey BRYLYAKOV <sergeybrylyakov@o-s-i.org>
2022-01-10 00:45:33 +0200

    header-bar

--- wp-content/themes/test-wp/header.php
+++ wp-content/themes/test-wp/header.php
@@ -10,6 +10,7 @@
 <div id="page" class="site">
 
 		<header>
+
             <div class="header-top">
                 <div class="socials container">
                     <div id="header_top" class="sidebar">
@@ -17,6 +18,62 @@
                     </div>
                 </div>
             </div>
-		</header><!-- #masthead -->
 
+            <div class="header-bottom">
+
+                <div class="container">
+
+                    <div class="header-bar">
+
+                        <div id="mobile_menu_open_toggler" class="mobile_menu_open_toggler">
+                            <span class="toggler-lines">
+                        </div>
+
+                        <div class="logo">
+                            <img src="<?php echo get_stylesheet_directory_uri().'/assets/img/logo.png' ?>" alt="logo" width="296" height="69">
+                        </div>
+
+                        <div class="header-search">
+                            <?php get_search_form(); ?>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div id="mobile-menu-main" class="mobile-menu">
+
+                    <div class="mobile-menu-wrapper">
+
+                        <div id="mobile_menu_close_toggler" class="mobile-menu-toggler">
+                            <span class="toggler-line one"></span>
+                            <span class="toggler-line two"></span>
+                        </div>
+
+                        <div class="mobile-menu-header">
+                            <div class="logo">
+                                <img src="<?php echo get_stylesheet_directory_uri().'/assets/img/logo.png' ?>" alt="logo" width="296" height="69">
+                            </div>
+                        </div>
+
+                        <div class="mobile-menu-content">
+                            <?php wp_nav_menu([
+                                'theme_location' => 'main',
+                            ]); ?>
+                        </div>
+
+                        <div class="mobile-menu-footer">
+                            <div class="mobile-menu-copyright">
+                                © 2021 <span class="color-accent">bBluePrintGaming</span><br>
+                                All rights reserved.
+                            </div>
+                        </div>
+
+                    </div><!--.mobile-menu-wrapper-->
+
+                </div><!-- #mobile-menu-main-->
+
+            </div><!--header-bottom-->
+
+		</header>
 	<div id="content" class="site-content">
--- wp-content/themes/test-wp/index.php
+++ wp-content/themes/test-wp/index.php
@@ -3,38 +3,6 @@
 <div class="content-area">
 	<main id="main" class="site-main">
 
-        <div id="mobile-menu-main" class="mobile-menu active">
-
-            <div class="mobile-menu-wrapper">
-
-                <div id="mobile_menu_close_toggler" class="mobile-menu-toggler">
-                    <span class="toggler-line one"></span>
-                    <span class="toggler-line two"></span>
-                </div>
-
-                <div class="mobile-menu-header">
-                    <div class="logo">
-                        <img src="<?php echo get_stylesheet_directory_uri().'/assets/img/logo.png' ?>" alt="logo" width="296" height="69">
-                    </div>
-                </div>
-
-                <div class="mobile-menu-content">
-                    <?php wp_nav_menu([
-                        'theme_location' => 'main',
-                    ]); ?>
-                </div>
-
-                <div class="mobile-menu-footer">
-                    <div class="mobile-menu-copyright">
-                        © 2021 <span class="color-accent">bBluePrintGaming</span><br>
-                        All rights reserved.
-                    </div>
-                </div>
-
-            </div><!--.mobile-menu-wrapper-->
-
-        </div><!-- #mobile-menu-main-->
-
 		<?php
         if ( have_posts() ) {
 
