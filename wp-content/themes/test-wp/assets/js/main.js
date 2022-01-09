5caad5a46321dd4e7ad48fb060d2eaa427652b62
Sergey BRYLYAKOV <sergeybrylyakov@o-s-i.org>
2022-01-09 14:09:58 +0200

    submenu control

--- wp-content/themes/test-wp/assets/js/main.js
+++ wp-content/themes/test-wp/assets/js/main.js
@@ -1,20 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
-    // single element
-    // if((e = document.querySelector("#mobile-menu-main")) !== null)
-        // e.classList.remove('active'); // Аналог выборки и присвоения класса
-        // e.onclick( function () {
 
-        // });
-    // many elements
-    Array.prototype.forEach.call(document.querySelectorAll("#form_error_message_frontend + div > div:last-child label"), function(e){
-        e.classList.add('last');
-    });
-
-    let mobile_menu_close_toggler = document.querySelector("#mobile_menu_close_toggler");
-    let mobile_menu_main = document.querySelector("#mobile-menu-main");
+    let mobile_menu_close_toggler = document.querySelector('#mobile_menu_close_toggler');
+    let mobile_menu_main = document.querySelector('#mobile-menu-main');
+    let mobile_menu_parent_items = document.querySelectorAll('#mobile-menu-main .menu-item-has-children > a');
 
     mobile_menu_close_toggler.addEventListener( 'click', function () {
         mobile_menu_main.classList.remove('active');
+    });
+
+    mobile_menu_parent_items.forEach(function (element) {
+        element.addEventListener ( 'click', function(e) {
+            if (element.parentElement.classList.contains('active')) {
+                element.parentElement.classList.remove('active');
+            } else {
+                e.preventDefault();
+                e.stopPropagation();
+                element.parentElement.classList.add('active');
+            }
+        });
     });
 
 });
