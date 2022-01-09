8a504eb85401926228ce6b57065fe652ab07ebfd
Sergey BRYLYAKOV <sergeybrylyakov@o-s-i.org>
2022-01-09 03:13:46 +0200

    remove emoji

--- wp-content/themes/test-wp/functions.php
+++ wp-content/themes/test-wp/functions.php
@@ -1,6 +1,26 @@
 <?php
+
+add_action( 'init', 'twp_init' );
+function twp_init() {
+	global $twp_version;
+	$twp_version = '0.1.1';
+}
+
+/**
+ * Disable the emoji's
+ */
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+
 add_action( 'wp_print_styles', 'twp_styles' );
 
 function twp_styles() {
-	wp_enqueue_style( 'main', get_stylesheet_uri() );
+	global $twp_version;
+	wp_enqueue_style( 'main', get_stylesheet_uri(), array(), $twp_version );
 }
+
+add_action ( 'wp_print_scripts', 'twp_scripts' );
+function twp_scripts() {
+	global $twp_version;
+}
+
