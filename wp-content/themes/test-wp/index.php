6ec7fdbfe47c39316f75ce3eee785f0d303384bd
Sergey BRYLYAKOV <sergeybrylyakov@o-s-i.org>
2022-01-09 12:02:55 +0200

    add menu to template

--- wp-content/themes/test-wp/index.php
+++ wp-content/themes/test-wp/index.php
@@ -1,68 +1,55 @@
-<?php get_header(); ?>
+<?php get_header();?>
 
 <div class="content-area">
 	<main id="main" class="site-main">
 
-    <div id="mobile-menu-main" class="mobile-menu active">
+        <div id="mobile-menu-main" class="mobile-menu active">
 
-        <div class="mobile-menu-wrapper">
+            <div class="mobile-menu-wrapper">
 
-            <div id="mobile_menu_close_toggler" class="mobile-menu-toggler">
-                <span class="toggler-line one"></span>
-                <span class="toggler-line two"></span>
-            </div>
-
-            <div class="mobile-menu-header">
-                <div class="logo">
-                    <img src="<?php echo get_stylesheet_directory_uri().'/assets/img/logo.png' ?>" alt="logo" width="296" height="69">
+                <div id="mobile_menu_close_toggler" class="mobile-menu-toggler">
+                    <span class="toggler-line one"></span>
+                    <span class="toggler-line two"></span>
                 </div>
-            </div>
 
-            <div class="mobile-menu-content">
-                <nav class="mobile-nav-menu">
-                    <ul>
-                        <li>Homepage</li>
-                        <li>Our team</li>
-                        <li>Top sites</li>
-                        <li class="menu-item-has-children">
-                            <a href="#">Programs</a>
-                            <ul class="sub-menu">
-                                <li id="menu-item-8" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-8"><a href="#">first child</a></li>
-                                <li id="menu-item-9" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-9"><a href="#">second child</a></li>
-                            </ul>
-                        </li>
-                        <li>Contact us</li>
-                        <li>Blog</li>
-                    </ul>
-                </nav>
-            </div>
-
-            <div class="mobile-menu-footer">
-                <div class="mobile-menu-copyright">
-                    © 2021 <span class="color-accent">bBluePrintGaming</span><br>
-                    All rights reserved.
+                <div class="mobile-menu-header">
+                    <div class="logo">
+                        <img src="<?php echo get_stylesheet_directory_uri().'/assets/img/logo.png' ?>" alt="logo" width="296" height="69">
+                    </div>
                 </div>
-            </div>
 
-        </div>
-    </div>
-</div>
+                <div class="mobile-menu-content">
+                    <?php wp_nav_menu([
+                        'theme_location' => 'main',
+                    ]); ?>
+                </div>
+
+                <div class="mobile-menu-footer">
+                    <div class="mobile-menu-copyright">
+                        © 2021 <span class="color-accent">bBluePrintGaming</span><br>
+                        All rights reserved.
+                    </div>
+                </div>
+
+            </div><!--.mobile-menu-wrapper-->
+
+        </div><!-- #mobile-menu-main-->
 
 		<?php
-	if ( have_posts() ) {
+        if ( have_posts() ) {
 
-		// Load posts loop.
-		while ( have_posts() ) {
-			the_post();
-			the_content();
-		}
+            // Load posts loop.
+            while ( have_posts() ) {
+                the_post();
+                the_content();
+            }
 
-	} else {
+        } else {
 
-		// If no content, include the "No posts found" template.
-		get_template_part( 'template-parts/content/content', 'none' );
+            // If no content, include the "No posts found" template.
+            get_template_part( 'template-parts/content/content', 'none' );
 
-	}
+        }
 		?>
 
 	</main><!-- .site-main -->
